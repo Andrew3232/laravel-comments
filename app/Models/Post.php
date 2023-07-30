b4fc1d5a8a8870258f8d9811c2bde5d75968011d
Andrew3232 <oleinitch@gmail.com>
2023-07-31 02:53:34 +0300

    add Post model

---
+++ app/Models/Post.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'title',
+        'description',
+    ];
+
+    protected $with = [
+        'comments'
+    ];
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class, 'post_id')->whereNull('parent_id');
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function getCreatedAt(): Carbon
+    {
+        return $this->created_at;
+    }
+}
