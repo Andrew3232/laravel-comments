f5252c4602835b9a1056c42457f3af731a63ef74
Andrew3232 <oleinitch@gmail.com>
2023-07-31 05:32:26 +0300

    add PostController

---
+++ app/Http/Controllers/Api/Post/PostController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api\Post;
+
+use App\Http\Controllers\Api\ApiController;
+use App\Http\Presenters\PostPresenter;
+use App\Models\Post;
+use Illuminate\Http\JsonResponse;
+
+class PostController extends ApiController
+{
+    /**
+     * Get all posts
+     *
+     * @param  PostPresenter  $presenter
+     * @return JsonResponse
+     */
+    public function index(PostPresenter $presenter): JsonResponse
+    {
+        $posts = Post::all();
+
+        return $this->successResponse($presenter->presentCollection($posts));
+    }
+
+    /**
+     * Get post
+     *
+     * @param  Post  $post
+     * @param  PostPresenter  $presenter
+     * @return JsonResponse
+     */
+    private function show(Post $post, PostPresenter $presenter): JsonResponse
+    {
+        return $this->successResponse($presenter->present($post));
+    }
+}
