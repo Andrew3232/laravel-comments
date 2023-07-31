db1d9da313fe143218af4f91880cefddc34c9ea9
Andrew3232 <oleinitch@gmail.com>
2023-07-31 07:44:42 +0300

    add PostTest

---
+++ tests/Feature/PostTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Post;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PostTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private mixed $post;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->post = Post::create([
+            'title' => 'test Title',
+            'description' => 'Test description',
+        ]);
+    }
+
+    public function testGetPostList(): void
+    {
+        $response = $this->getJson(route('posts.index'));
+
+        $response->assertOk()
+            ->assertJsonStructure([
+                '*' => [
+                    'id',
+                    'title',
+                    'description',
+                    'created_at',
+                ]
+            ])
+            ->assertJsonFragment([
+                'id' => $this->post->id,
+                'title' => $this->post->title,
+                'description' => $this->post->description,
+            ]);
+    }
+
+    public function testGetPost(): void
+    {
+        $response = $this->getJson(route('posts.show', ['id' => $this->post->id]));
+
+        $response->assertOk()
+            ->assertJsonStructure([
+                'id',
+                'title',
+                'description',
+                'created_at',
+            ])
+            ->assertJsonFragment([
+                'id' => $this->post->id,
+                'title' => $this->post->title,
+                'description' => $this->post->description,
+            ]);
+    }
+}
